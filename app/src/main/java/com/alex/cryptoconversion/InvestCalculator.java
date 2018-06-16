2863ed7ca6a67424e4b0040509725735fdb1ac8f
Alexdgg <ciortea_alex10@yahoo.com>
2018-06-16 18:54:47 +0300

    Df

---
+++ app/src/main/java/com/alex/cryptoconversion/InvestCalculator.java
@@ -0,0 +1,196 @@
+package com.alex.cryptoconversion;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class InvestCalculator extends AppCompatActivity {
+
+    private final String BTC_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCRON";
+    private final String XRP_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/XRPRON";
+    private final String ZEC_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/ZECRON";
+    private final String ETH_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/ETHRON";
+    private final String LTC_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/LTCRON";
+    private final String XMR_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/XMRRON";
+
+
+    EditText inputAmount;
+    TextView rezultat;
+    Button btc, xrp, zec, xmr, ltc, eth;
+
+
+    private double cantitatePermisa, sumaDisponibila;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.invest_calculator);
+
+        inputAmount = (EditText) findViewById(R.id.input_box);
+        rezultat = (TextView) findViewById(R.id.result_box);
+        btc = (Button) findViewById(R.id.button10);
+        xrp = (Button) findViewById(R.id.button9);
+        zec = (Button) findViewById(R.id.button11);
+        xmr = (Button) findViewById(R.id.button12);
+        ltc = (Button) findViewById(R.id.button13);
+        eth = (Button) findViewById(R.id.button14);
+        configureMainButton();
+
+
+        btc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumaDisponibila = Double.valueOf(inputAmount.getText().toString());
+                apiRequest(BTC_URL);
+                String s = " Bitcoin's";
+                rezultat.setText(s);
+            }
+        });
+
+        xrp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumaDisponibila = Double.valueOf(inputAmount.getText().toString());
+                apiRequest(XRP_URL);
+                String s = " Ripple's";
+                rezultat.setText(s);
+            }
+        });
+
+        zec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumaDisponibila = Double.valueOf(inputAmount.getText().toString());
+                apiRequest(ZEC_URL);
+                String s = " Zcash's";
+                rezultat.setText(s);
+            }
+        });
+
+        xmr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumaDisponibila = Double.valueOf(inputAmount.getText().toString());
+                apiRequest(XMR_URL);
+                String s = " Monero's";
+                rezultat.setText(s);
+            }
+        });
+
+        ltc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumaDisponibila = Double.valueOf(inputAmount.getText().toString());
+                apiRequest(LTC_URL);
+                String s = " Litecoin's";
+                rezultat.setText(s);
+            }
+        });
+
+        eth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumaDisponibila = Double.valueOf(inputAmount.getText().toString());
+                apiRequest(ETH_URL);
+                String s = " Ethereum's";
+                rezultat.setText(s);
+            }
+        });
+
+    }
+
+
+
+    public void apiRequest(String url) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                Log.d("bitcoin", "JSON: " + response.toString());
+
+                try{
+                    int price = response.getInt("last");
+                    Log.v("bitcoin", "pretul in INT: " + price);
+                    NumberFormat nf = NumberFormat.getNumberInstance();
+                    nf.setGroupingUsed(true);
+                    nf.setMaximumFractionDigits(4);
+                    nf.setMinimumIntegerDigits(1);
+
+
+                    if(sumaDisponibila < price) {
+                        cantitatePermisa = sumaDisponibila / price;
+                    }
+                    if(sumaDisponibila >= price) {
+                        cantitatePermisa = price / sumaDisponibila;
+                    }
+
+                    rezultat.setText(String.valueOf(nf.format(cantitatePermisa)));
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("Bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("Bitcoin", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+
+            }
+        });
+
+    }
+
+
+
+    private void configureMainButton() {
+        Button goToMainButton = (Button) findViewById(R.id.backBackButton);
+        goToMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/alex/cryptoconversion/TrackerActivity.java
@@ -0,0 +1,218 @@
+import android.app.DownloadManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.alex.cryptoconversion.AppController;
+import com.alex.cryptoconversion.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+//import com.alex.cryptoconversion;
+
+public class TrackerActivity extends AppCompatActivity {
+
+   // BarChart barChart;
+   // ArrayList<String> dates;
+   // Random random;
+   // ArrayList<BarEntry> barEntries;
+
+    ArrayList<Entry> x;
+    ArrayList<String> y;
+    private LineChart mChart;
+    public String TAG = "YOUR CLASS NAME";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tracking_layout);
+
+        configureBackButton();
+
+        //barChart = (BarChart) findViewById(R.id.bar_chart);
+
+        //createRandomBarGraph("2018/05/05" , "2018/06/01");
+
+
+        x = new ArrayList<Entry>();
+        y = new ArrayList<String>();
+        mChart = (LineChart) findViewById(R.id.chart1);
+        mChart.setDrawGridBackground(false);
+        mChart.setDescription("");
+        mChart.setTouchEnabled(true);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setPinchZoom(true);
+       // mChart.setMarkerView(mv);
+        XAxis xl = mChart.getXAxis();
+        xl.setAvoidFirstLastClipping(true);
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setInverted(true);
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+        Legend l = mChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+
+
+
+    }
+
+
+    private void drawChart() {
+
+        String tag_string_req = "req_chart";
+
+        StringRequest strReq = new StringRequest(DownloadManager.Request.Method.POST, "YOUR URL",
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d(TAG, "Response: " + response);
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String id = jsonObject.getString("id");
+                            JSONArray jsonArray = jsonObject.getJSONArray("measurements");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+
+                                int value = jsonObject.getInt("value");
+                                String date = jsonObject.getString("time");
+                                x.add(new Entry(value, i));
+                                y.add(date);
+
+                            }
+                            LineDataSet set1 = new LineDataSet(x, "NAV Data Value");
+                            set1.setLineWidth(1.5f);
+                            set1.setCircleRadius(4f);
+                            LineData data = new LineData(y, set1);
+                            mChart.setData(data);
+                            mChart.invalidate();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+            }
+        });
+        strReq.setRetryPolicy(new RetryPolicy() {
+
+            @Override
+            public void retry(VolleyError arg0) throws VolleyError {
+            }
+
+            @Override
+            public int getCurrentTimeout() {
+                return 0;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 0;
+            }
+        });
+        strReq.setShouldCache(false);
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+
+
+
+
+
+    /*public void createRandomBarGraph(String Date1, String Date2) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+        try {
+            Date date1 = simpleDateFormat.parse(Date1);
+            Date date2 = simpleDateFormat.parse(Date2);
+
+            Calendar mDate1 = Calendar.getInstance();
+            Calendar mDate2 = Calendar.getInstance();
+
+            mDate1.clear();
+            mDate2.clear();
+
+            mDate1.setTime(date1);
+            mDate2.setTime(date2);
+
+            dates = new ArrayList<>();
+            dates = getList(mDate1, mDate2);
+
+            barEntries = new ArrayList<>();
+            float max = 0f;
+            float value = 0f;
+            random = new Random();
+            for(int j = 0; j < dates.size(); j++) {
+                max = 100f;
+                value = random.nextFloat()*max;
+                barEntries.add(new BarEntry(value, j));
+            }
+
+        }catch(ParseException e) {
+            e.printStackTrace();
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Dates");
+        BarData barData = new BarData(dates, barDataSet);
+        barChart.setData(barData);
+        barChart.setDescription("CryptoCurrency");
+
+    }
+
+    public ArrayList<String> getList(Calendar startDate, Calendar endDate) {
+
+        ArrayList<String> list = new ArrayList<>();
+        while(startDate.compareTo(endDate) <= 0) {
+            list.add(getDate(startDate));
+            startDate.add(Calendar.DAY_OF_MONTH,1);
+        }
+
+        return list;
+
+    }
+
+    public String getDate(Calendar cld) {
+        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/" + cld.get(Calendar.DAY_OF_MONTH);
+
+        try{
+            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
+            curDate = new SimpleDateFormat("yyy//MM//dd").format(date);
+
+        }catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return curDate;
+    }*/
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+
+}
